6be7e24dbac707bad85c14dacf8bd74f304e25f6
Cory Dominguez <corydominguez@gmail.com>
2014-12-11 08:51:55 -0800

    Vagrant and provisioning edits

--- provisioning/lurker.sh
+++ provisioning/lurker.sh
@@ -6,12 +6,12 @@
 echo "Provisioning..."
 sudo apt-get -y update
 sudo apt-get -y install build-essential libssl-dev
-
+sudo apt-get -y install git
 # install go
 wget https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.3.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc 
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc 
+echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc 
+sudo chown -R vagrant:vagrant /home/vagrant/go
 echo "Good Hunting"
